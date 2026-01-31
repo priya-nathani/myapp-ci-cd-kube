5c2aa826a8d2068a4bf2f1e6a022d9b710e77a00
priya-nathani <priyanathani38@gmail.com>
2026-01-31 19:11:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App(){
     return (
         <div>
             <h1>Hello World</h1>
-            <H2>SUCCESSSSS YAYYAYYAY!!!!</H2>
+            <h2>SUCCESSSSS YAYYAYYAY!!!!</h2>
         </div>
     )
 }
